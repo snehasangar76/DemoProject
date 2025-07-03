ffc8072dbc287884094dda11dafd697a8989ee5f
Sneha <snehasangar76@gmail.com>
2025-07-03 15:40:56 +0530

    added numbers from 1 to 12

--- Test/src/com/Sneha/Sample.java
+++ Test/src/com/Sneha/Sample.java
@@ -3,7 +3,7 @@ package com.Sneha;
 public class Sample {
 	
 	public static void main(String[] args) {
-		for(int i=1; i<=10; i++) {
+		for(int i=1; i<=12; i++) {
 			System.out.println(i);
 		}
 	}
